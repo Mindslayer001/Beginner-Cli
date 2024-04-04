dc70137cb6d1cf1fd9f9ef3daf320503649d74b3
Mani Sankar Chintagunti <manisankarchintagunti@gmail.com>
2024-04-04 12:22:59 +0530

    removed path variable bug in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Install dependencies using Poetry
+current_dir=$(pwd)
 echo "Installing Dependencies"
 poetry install
 
@@ -7,7 +8,7 @@ poetry install
 echo "trying to add alias for easier access"
 if [ -f "$HOME/.zshrc" ]; then
     # If .zshrc exists, add the alias to .zshrc
-    echo "alias beg-cli='poetry run python /path/to/beg_cli/argsparser.py'" >> "$HOME/.zshrc"
+    echo "alias beg-cli='poetry run python $current_dir/beg_cli/argsparser.py'" >> "$HOME/.zshrc"
     echo "Alias added to .zshrc"
 else
     # If .zshrc doesn't exist, add the alias to .bashrc
