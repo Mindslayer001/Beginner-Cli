bab62dfb0a291e8ed1f7f4fd43eed1a6aa3438e1
Mani Sankar Chintagunti <manisankarchintagunti@gmail.com>
2024-04-04 14:57:07 +0530

    optimized path in setup.py

--- setup.sh
+++ setup.sh
@@ -8,11 +8,10 @@ pip install -r "$current_dir/requirements.txt" --break-system-packages
 echo "trying to add alias for easier access"
 if [ -f "$HOME/.zshrc" ]; then
     # If .zshrc exists, add the alias to .zshrc
-    
     echo "alias beg-cli='python $current_dir/beg_cli/argsparser.py'" >> "$HOME/.zshrc"
     echo "Alias added to .zshrc"
 else
     # If .zshrc doesn't exist, add the alias to .bashrc
-    echo "alias beg-cli='python /path/to/beg_cli/argsparser.py'" >> "$HOME/.bashrc"
+    echo "alias beg-cli='python "$current_dir/beg_cli/argsparser.py"'" >> "$HOME/.bashrc"
     echo "Alias added to .bashrc"
 fi
