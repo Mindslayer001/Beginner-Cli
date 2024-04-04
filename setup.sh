614cc39adf3fc97cc8436617f6272440b70befc8
Mani Sankar Chintagunti <manisankarchintagunti@gmail.com>
2024-04-04 15:01:29 +0530

    changed beg-cli to begcli

--- setup.sh
+++ setup.sh
@@ -8,10 +8,10 @@ pip install -r "$current_dir/requirements.txt" --break-system-packages
 echo "trying to add alias for easier access"
 if [ -f "$HOME/.zshrc" ]; then
     # If .zshrc exists, add the alias to .zshrc
-    echo "alias beg-cli='python $current_dir/beg_cli/argsparser.py'" >> "$HOME/.zshrc"
+    echo "alias begcli='python $current_dir/beg_cli/argsparser.py'" >> "$HOME/.zshrc"
     echo "Alias added to .zshrc"
 else
     # If .zshrc doesn't exist, add the alias to .bashrc
-    echo "alias beg-cli='python "$current_dir/beg_cli/argsparser.py"'" >> "$HOME/.bashrc"
+    echo "alias begcli='python "$current_dir/beg_cli/argsparser.py"'" >> "$HOME/.bashrc"
     echo "Alias added to .bashrc"
 fi
