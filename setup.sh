c38d43c1c6f6ef6408d289d6a2ca8ac048af4618
Mani Sankar Chintagunti <manisankarchintagunti@gmail.com>
2024-04-04 14:35:16 +0530

    again added poetry and published in pypi

--- setup.sh
+++ setup.sh
@@ -2,12 +2,19 @@
 # Install dependencies using Poetry
 current_dir=$(pwd)
 echo "Installing Dependencies"
-pip install -r requirements.txt
+if [ ! -d "$HOME/venv" ]; then
+    echo "Creating Virtual environment in HOME directory"
+    cd "$HOME"
+    python -m venv venv;
+fi
+source "$HOME/venv/bin/activate"
+pip install -r "$current_dir/requirements.txt"
 
 # Check if .zshrc exists
 echo "trying to add alias for easier access"
 if [ -f "$HOME/.zshrc" ]; then
     # If .zshrc exists, add the alias to .zshrc
+    
     echo "alias beg-cli='python $current_dir/beg_cli/argsparser.py'" >> "$HOME/.zshrc"
     echo "Alias added to .zshrc"
 else
