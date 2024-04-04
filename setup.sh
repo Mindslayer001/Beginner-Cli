96938dd154d69779674c238e02deba4272747c90
Mani Sankar Chintagunti <manisankarchintagunti@gmail.com>
2024-04-04 13:08:27 +0530

    removed poetry

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 # Install dependencies using Poetry
 current_dir=$(pwd)
 echo "Installing Dependencies"
-poetry install
+pip install -r requirements.txt
 
 # Check if .zshrc exists
 echo "trying to add alias for easier access"
 if [ -f "$HOME/.zshrc" ]; then
     # If .zshrc exists, add the alias to .zshrc
-    echo "alias beg-cli='poetry run python $current_dir/beg_cli/argsparser.py'" >> "$HOME/.zshrc"
+    echo "alias beg-cli='python $current_dir/beg_cli/argsparser.py'" >> "$HOME/.zshrc"
     echo "Alias added to .zshrc"
 else
     # If .zshrc doesn't exist, add the alias to .bashrc
-    echo "alias beg-cli='poetry run python /path/to/beg_cli/argsparser.py'" >> "$HOME/.bashrc"
+    echo "alias beg-cli='python /path/to/beg_cli/argsparser.py'" >> "$HOME/.bashrc"
     echo "Alias added to .bashrc"
 fi
