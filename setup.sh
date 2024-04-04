465b178b8248e586efcf7f80539c39ecb4eef531
Mani Sankar Chintagunti <manisankarchintagunti@gmail.com>
2024-04-04 12:20:26 +0530

    Added user friendly messages in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Install dependencies using Poetry
+echo "Installing Dependencies"
 poetry install
 
 # Check if .zshrc exists
+echo "trying to add alias for easier access"
 if [ -f "$HOME/.zshrc" ]; then
     # If .zshrc exists, add the alias to .zshrc
-    echo "alias mycli='poetry run python /path/to/beg_cli/argsparser.py'" >> "$HOME/.zshrc"
+    echo "alias beg-cli='poetry run python /path/to/beg_cli/argsparser.py'" >> "$HOME/.zshrc"
     echo "Alias added to .zshrc"
 else
     # If .zshrc doesn't exist, add the alias to .bashrc
-    echo "alias mycli='poetry run python /path/to/beg_cli/argsparser.py'" >> "$HOME/.bashrc"
+    echo "alias beg-cli='poetry run python /path/to/beg_cli/argsparser.py'" >> "$HOME/.bashrc"
     echo "Alias added to .bashrc"
 fi
