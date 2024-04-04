1864d1eb0a923720a8c395513c93d90cf7f8d765
Mani Sankar Chintagunti <manisankarchintagunti@gmail.com>
2024-04-04 15:06:24 +0530

    redirected pip install details while runnig to /dev/null

--- setup.sh
+++ setup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-# Install dependencies using Poetry
+
 current_dir=$(pwd)
 echo "Installing Dependencies"
-pip install -r "$current_dir/requirements.txt" --break-system-packages
+pip install -r "$current_dir/requirements.txt" --break-system-packages > /dev/null 2>&1
 
 # Check if .zshrc exists
-echo "trying to add alias for easier access"
+echo "Trying to add alias for easier access"
 if [ -f "$HOME/.zshrc" ]; then
     # If .zshrc exists, add the alias to .zshrc
-    echo "alias begcli='python $current_dir/beg_cli/argsparser.py'" >> "$HOME/.zshrc"
+    echo "alias begcli='python \"$current_dir/beg_cli/argsparser.py\"'" >> "$HOME/.zshrc"
     echo "Alias added to .zshrc"
 else
     # If .zshrc doesn't exist, add the alias to .bashrc
-    echo "alias begcli='python "$current_dir/beg_cli/argsparser.py"'" >> "$HOME/.bashrc"
+    echo "alias begcli='python \"$current_dir/beg_cli/argsparser.py\"'" >> "$HOME/.bashrc"
     echo "Alias added to .bashrc"
 fi
+
+# Set executable permission for argsparser.py
+chmod +x "$current_dir/beg_cli/argsparser.py"
+
+echo "try typing 'begcli -i ls' for starters"
